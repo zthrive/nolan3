ae9a06422c2c8113fe2df22777607e4519e9ff82
Zoran Milutinovic <zoran.milutinovic.999@gmail.com>
2022-05-05 16:31:25 +1000

    sss

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -83,8 +83,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/ic_launcher.png": "41cc3beaa663c89ec59216832485bc96",
 "icons/ms-icon-310x310.png": "1e3ed29f39b2e36dd2ba73877666e4ec",
-"index.html": "001bb6e494b3d878018ac51e207d177b",
-"/": "001bb6e494b3d878018ac51e207d177b",
+"index.html": "b499b433b0c1696bc373ead90971d428",
+"/": "b499b433b0c1696bc373ead90971d428",
 "main.dart.js": "8a94f8157e6772cc3aef13909efff3f4",
 "manifest.json": "ccfc140e0166b78756860f9cf85b6613",
 "version.json": "379a381ec022ca41443e4228b0cf14cb"
